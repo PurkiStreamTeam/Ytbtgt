3946350449af0b08f482a4c1dde3f4d2743c26a4
Ellie <132861889+PurkiStreamTeam@users.noreply.github.com>
2025-02-14 20:08:52 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('play', () => {
+        socket.broadcast.emit('play'); // Notify all other users to play
+    });
+
+    socket.on('pause', () => {
+        socket.broadcast.emit('pause'); // Notify all other users to pause
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
